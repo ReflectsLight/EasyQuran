07e58d07c2403c996dbf72ca69a4157d1cf9725f
0x1eef <0x1eef@protonmail.com>
2024-11-01 22:40:27 -0300

    Remove unneccessary div

--- src/js/components/Head.tsx
+++ src/js/components/Head.tsx
@@ -9,21 +9,19 @@ export function Head({ children }: Props) {
   const { locale } = useContext(SettingsContext);
   return (
     <header className="flex flex-col h-20 mt-4 mb-4">
-      <div className="flex flex-col">
-        <div className="flex items-center justify-center">
-          <a
-            data-testid="h1"
-            href={`/${locale.name}/`}
-            className="flex rounded justify-center p-3 m-0 mb-4 w-full no-underline font-semibold color-secondary text-1xl"
-          >
-            {children}
-          </a>
-        </div>
-        <nav className="flex flex-row justify-between text-lg">
-          <LanguageSelect />
-          <ThemeSelect />
-        </nav>
+      <div className="flex items-center justify-center">
+        <a
+          data-testid="h1"
+          href={`/${locale.name}/`}
+          className="flex rounded justify-center p-3 m-0 mb-4 w-full no-underline font-semibold color-secondary text-1xl"
+        >
+          {children}
+        </a>
       </div>
+      <nav className="flex flex-row justify-between text-lg">
+        <LanguageSelect />
+        <ThemeSelect />
+      </nav>
     </header>
   );
 }
